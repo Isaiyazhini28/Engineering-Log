ea96960b9f318434776c6c0bf25893f68e0c7b81
Isaiyazhini28 <isai12d25@gmail.com>
2024-10-04 10:28:44 +0530

    comment zod,dashboard status

--- UI/src/pages/DynamicFormComp.tsx
+++ UI/src/pages/DynamicFormComp.tsx
@@ -2,7 +2,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { createZodSchema } from "@/pages/ZodSchema";
+import { createZodSchema, ReadingSubmitSchema, SubmitFormType } from "@/pages/ZodSchema";
 import {
   Form,
   FormControl,
@@ -13,34 +13,55 @@ import {
 
 import {
   useDynamicFormInsertStore,
+  useFieldsStore,
   useSelectedLocationAndTransactionIDStore,
 } from "@/store/store";
 import { useEffect, useState } from "react";
-import { UpdateFieldvalueAPI } from "@/services/api";
+import {
+  UpdateFieldvalueAPI,
+  UpdateTransactionStatusAPI,
+} from "@/services/api";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { number, z } from "zod";
 import { toast } from "react-toastify";
 import { Textarea } from "@/components/ui/textarea";
 import { useGetFieldsBasedOnLocationIdAPIQuery } from "@/services/query";
+import { isValid } from "date-fns";
+import {useNavigate } from "react-router-dom";
 
 export function DynamicFormComp() {
+  const navigate = useNavigate();
   const queryClient = useQueryClient();
   const selectedLoactionId = useSelectedLocationAndTransactionIDStore(
     (state) => state
   );
+  const s_LoactionId = useSelectedLocationAndTransactionIDStore(
+    (state) => state.LocationId
+  );
   const { data: FieldsData } = useGetFieldsBasedOnLocationIdAPIQuery({
-    locationId: selectedLoactionId.LocationId,
+    locationId: s_LoactionId,
   });
+ 
 
   const [dailyFieldsArray, setDailyFieldsArray] = useState([]);
   const [monthlyFieldsArray, setMonthlyFieldsArray] = useState([]);
   const [MonthlyFieldsFlatArray, setMonthlyFieldsFlatArray] = useState([]);
   const dynamicSchema = createZodSchema(dailyFieldsArray);
   type DynamicFormType = z.infer<typeof dynamicSchema>;
+
+  const submitForm = useForm<SubmitFormType>({
+    resolver: zodResolver(ReadingSubmitSchema),
+    mode: "onChange",
+    defaultValues: {
+      comment: "",
+    },
+  });
   const dynamicForm = useForm<DynamicFormType>({
     resolver: zodResolver(dynamicSchema),
     mode: "onChange",
   });
+
+  const dynamicFormWatch = dynamicForm.watch();
   const UserDetails = JSON.parse(sessionStorage.getItem("UserDetails"));
   const FiledStroe = useDynamicFormInsertStore();
   const flat: any = ({ childFields = [], ...o }) => [
@@ -60,24 +81,16 @@ export function DynamicFormComp() {
 
   useEffect(() => {
     if (dailyFieldsArray.length > 0) {
-      dailyFieldsArray.map((field:any)=>{
-        dynamicForm.setValue(field.subFieldId!==null?field.subFieldName+"/"+field.fieldName :field.fieldName,field.value)
-      })
+      dailyFieldsArray.map((field: any) => {
+        dynamicForm.setValue(
+          field.subFieldId !== null
+            ? field.subFieldName + "/" + field.fieldName
+            : field.fieldName,
+          field.value
+        );
+      });
     }
   }, [dailyFieldsArray]);
-
-  const onSubmit = (data: DynamicFormType) => {
-    console.log(data, "form Data");
-    
-    // let input = {
-    //   fields: FiledStroe.Fields,
-    //   locationId: selectedLoactionId,
-    //   empId: "NPI3838",
-    //   remark: "Test",
-
-    // };
-    // DynamicForm(input);
-  };
 
   const { mutate: UpdateFieldvalue } = useMutation({
     mutationFn: (data: any) => UpdateFieldvalueAPI(data),
@@ -89,6 +102,17 @@ export function DynamicFormComp() {
       queryClient.refetchQueries({ queryKey: ["FieldsBasedOnLocationId"] });
     },
   });
+  const { mutate: UpdateTransactionStatus } = useMutation({
+    mutationFn: (data: any) => UpdateTransactionStatusAPI(data),
+    onError: (e) => {
+      console.log(e, "Error");
+    },
+    onSuccess: () => {
+      toast.success("Submited Successfully!");
+      navigate("/")
+    },
+  });
+
 
   const onValueChange = async (
     event: any,
@@ -111,6 +135,21 @@ export function DynamicFormComp() {
     }
   };
 
+  const onSubmitClick = async () => {
+    const DynamicFormResult = await dynamicForm.trigger();
+    const SubmitFormResult = await submitForm.trigger();
+    if (DynamicFormResult && SubmitFormResult) {
+      alert("inner Call");
+      let submit = {
+        transactionId: selectedLoactionId.transactionId,
+        submitted: true,
+        employeeId: UserDetails.id,
+        remark: submitForm.getValues("comment"),
+      };
+      UpdateTransactionStatus(submit);
+    }
+  };
+
   const formLoad = (
     fieldData: any,
     parentPath = "",
@@ -121,12 +160,17 @@ export function DynamicFormComp() {
       ? `${parentPath}.${fieldData.fieldName}`
       : fieldData.fieldName;
     return (
-      <div className="grid grid-cols-1 gap-2" key={fieldData.fieldName}>
+      <div
+        className="grid grid-cols-1 gap-2 mr-5 ml-5"
+        key={fieldData.fieldName}
+      >
         {fieldData.childFields && fieldData.childFields.length > 0 ? (
-          <div className="col-span-3 grid grid-cols-1 items-start">
+          <div className="col-span-3 grid grid-cols-1 items-start mr-5 ml-5">
             <div className="grid grid-cols-6 gap-2">
               <label className="w-full h-full flex items-center justify-start text-white">
-                {fieldData.fieldName}
+                {fieldData.subFieldId !== null
+                  ? fieldData.subFieldName
+                  : fieldData.fieldName}
               </label>
             </div>
             <div className="col-span-1 grid grid-cols-1 gap-2">
@@ -138,7 +182,9 @@ export function DynamicFormComp() {
         ) : (
           <div className="col-span-3 grid grid-cols-6 gap-2 items-center">
             <label className="w-full h-full flex items-center justify-start text-white">
-              {fieldData.fieldName}
+              {fieldData.subFieldId !== null
+                ? fieldData.subFieldName
+                : fieldData.fieldName}
             </label>
             <div>
               <div className="w-full max-h-[40px] bg-indigo-950 text-white border-indigo-900 border rounded-[5px] p-2">
@@ -200,45 +246,57 @@ export function DynamicFormComp() {
   };
 
   return (
-    <div className="flex-1 flex flex-col h-full w-full bg-slate-300">
-      <div className="text-2xl font-bold text-center bg-yellow-100 h-12">
+    <div className="flex-1 flex flex-col h-full w-full bg-yellow-100">
+      <div className="text-2xl font-bold text-center bg-yellow-100 h-12 mr-2 ml-2">
         DAILY READINGS
       </div>
-      <div className="flex-1 overflow-y-auto overflow-x-hidden bg-indigo-950">
+      <div className="flex-1 overflow-y-auto overflow-x-hidden bg-indigo-950 mr-2 ml-2">
         <Form {...dynamicForm}>
-          <form onSubmit={dynamicForm.handleSubmit(onSubmit)}>
-            <div className="grid grid-cols-1 gap-2">
-              <div className="sticky top-0 z-10 grid grid-cols-6 font-semibold bg-yellow-400 text-black">
-                <div className="p-2">LABEL NAME</div>
-                <div className="p-2">LAST READING</div>
-                <div className="p-2">CURRENT READING</div>
-                <div className="p-2">DIFFERENCE</div>
-                <div className="p-2">MTD AVERAGE</div>
-                <div className="p-2">LAST MONTH AVERAGE</div>
-              </div>
-              {dailyFieldsArray?.map((item) => formLoad(item))}
-              {monthlyFieldsArray?.map((item) => formLoad(item))}
-              <Textarea
-                className="pr-8 ml-3 mt-3  bg-slate-100 w-full"
-                placeholder="Add a comment..."
-              ></Textarea>
-              <div className="flex justify-end mt-3">
-                <Button
-                  type="submit"
-                  className="flex justify-center items-center w-32 mr-4 bg-yellow-300 text-black hover:bg-yellow-100"
-                >
-                  Submit
-                </Button>
-              </div>
+          {/* <form onSubmit={dynamicForm.handleSubmit(onSubmit)}> */}
+          <div className="grid grid-cols-1 gap-2 ">
+            <div className="sticky top-0 z-10 grid grid-cols-6 font-semibold bg-yellow-400 text-black">
+              <div className="p-2">LABEL NAME</div>
+              <div className="p-2">LAST READING</div>
+              <div className="p-2">CURRENT READING</div>
+              <div className="p-2">DIFFERENCE</div>
+              <div className="p-2">MTD AVERAGE</div>
+              <div className="p-2">LAST MONTH AVERAGE</div>
             </div>
-          </form>
+            {dailyFieldsArray?.map((item) => formLoad(item))}
+            {monthlyFieldsArray?.map((item) => formLoad(item))}
+          </div>
+
+          {/* </form> */}
+        </Form>
+        <Form {...submitForm}>
+          <FormField
+            control={submitForm.control}
+            name={"comment"}
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Textarea
+                    className="pr-8 ml-3 mt-3 "
+                    placeholder="Add a comment..."
+                    style={{ width: "1165px", height: "100px" }}
+                    {...field}
+                  ></Textarea>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex justify-end mt-2 mb-5">
+            <Button
+              type="submit"
+              className="flex justify-center items-center w-32 mr-4 bg-yellow-300 text-black hover:bg-yellow-100"
+              onClick={onSubmitClick}
+            >
+              Submit
+            </Button>
+          </div>
         </Form>
       </div>
     </div>
   );
-}
-function useGetFieldsBasedOnLocationIdAPIQueryI(arg0: { LocationId: number }): {
-  data: any;
-} {
-  throw new Error("Function not implemented.");
 }
